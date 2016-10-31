c10d81c1f3229d33fb26d85c139bbfa2740e44ec
zhangxiaoyong <1064938480@qq.com>
2016-10-31 15:13:54 +0800

    Http接口请求和调用demo

---
+++ http接口程序demo/VipJump/vipjump/test.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VipJump
+{
+    public partial class test : System.Web.UI.Page
+    {
+        private string urlnode = ConfigurationSettings.AppSettings["urlnode"];
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(string .IsNullOrEmpty (urlnode))
+            {
+                urlnode ="jump";
+            }
+            try
+            {
+                string res = GetPost();
+                Response.Write(res);
+            }
+            catch (Exception ex)
+            {
+                Response.Write("异常："+ex.Message);
+            }
+        }
+
+        public string GetStringURLPara()
+        {
+            if (Request.QueryString[urlnode] != null)
+            {
+                string method = Request.QueryString[urlnode].ToString();
+                return method;
+            }
+            return "";
+        }
+
+        public string GetPost()
+        {
+            if ("POST" == Request.RequestType)
+            {
+                System.IO.Stream sm = Request.InputStream;//获取post正文
+                int len = (int)sm.Length;//post数据长度
+                byte[] inputByts = new byte[len];//字节数据,用于存储post数据
+                sm.Read(inputByts, 0, len);//将post数据写入byte数组中
+                sm.Close();//关闭IO流
+                //**********下面是把字节数组类型转换成字符串**********
+                string data = Encoding.GetEncoding("UTF-8").GetString(inputByts);//转为unicode编码
+                //data = Server.UrlDecode(data);//url参数用到.
+                return data;
+            }
+            else
+                return null;
+        }
+
+
+     
+
+    }
+}
---
+++ 调用http接口/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ss = "hello world!";
+            Encoding encoding = Encoding.GetEncoding("utf-8");
+            byte[] bytesToPost = encoding.GetBytes(ss);
+            string res = Post("http://localhost:23094/test.aspx", bytesToPost);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+
+        private static string Post(string url, byte[] bytesToPost)
+        {
+            if (String.IsNullOrEmpty(url))
+                return "url参数为空值";
+            if (bytesToPost == null)
+                return "post数据为空值";
+            string ResponseString = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            System.Net.ServicePointManager.DefaultConnectionLimit = 50;
+            request.KeepAlive = false;
+            request.Method = "POST";
+            request.ContentType = "text/xml";//提交xml   
+            request.ContentLength = bytesToPost.Length;
+            Stream writer = request.GetRequestStream();
+            writer.Write(bytesToPost, 0, bytesToPost.Length);
+            HttpWebResponse HttpWebRespon = (HttpWebResponse)request.GetResponse();
+            StreamReader myStreamReader = new StreamReader(HttpWebRespon.GetResponseStream(), Encoding.UTF8);
+            ResponseString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+
+            writer.Flush();
+            if (writer != null)
+            {
+                writer.Close();
+            }
+            if (request != null)
+            {
+                request.Abort();
+            }
+            return ResponseString;
+        }
+    }
+}
